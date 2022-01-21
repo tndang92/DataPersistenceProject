ba7f5ee0f605ea6e26a291e0a44ad0466658b7ad
TNDang <66015693+tndang92@users.noreply.github.com>
2022-01-21 07:48:45 -0500

    Update 2022.01.21.01
    Added Title to Menu UI, adjusted layout of the menu, then added Start and Quit button functionality to play the game, then exit play mode in editor or close the game at runtime.

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif 
+
+[DefaultExecutionOrder(1000)]
+
+public class MenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
